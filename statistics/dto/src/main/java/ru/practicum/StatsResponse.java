0a710a010ad43d78668a29b8e99243d021064bbd
Nastya <nimalyshevann@gmail.com>
2024-07-02 18:08:07 +0300

    fix: docker-compose.yml

--- statistics/dto/src/main/java/ru/practicum/StatsResponse.java
+++ statistics/dto/src/main/java/ru/practicum/StatsResponse.java
@@ -4,15 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StatsResponse {
-    //@NotNull
     private String app;
-    //@NotNull
     private String uri;
     private Long hits;
 }
