22c3290c3bfb170ec33150f1a7f9dd900b6dc351
Nastya <nimalyshevann@gmail.com>
2024-07-12 11:50:47 +0300

    fix: checkstyle

--- ewm-main-service/src/main/java/ru/practicum/events/publicar/PublicEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/events/publicar/PublicEventController.java
@@ -2,12 +2,10 @@ package ru.practicum.events.publicar;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.StatsClient;
 import ru.practicum.events.dto.EventFullDto;
 import ru.practicum.events.dto.EventShortDto;
 import ru.practicum.events.dto.GetEventsPublicRequest;
