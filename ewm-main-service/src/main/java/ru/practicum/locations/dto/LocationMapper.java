73338a60ace2651629504c64c88dae27f286d571
Nastya <nimalyshevann@gmail.com>
2024-07-17 18:05:51 +0300

    feat: локации

--- ewm-main-service/src/main/java/ru/practicum/locations/dto/LocationMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/locations/dto/LocationMapper.java
@@ -6,4 +6,6 @@ import ru.practicum.locations.model.Location;
 @Mapper(componentModel = "spring")
 public interface LocationMapper {
     Location toLocation(LocationDto location);
+    LocationFullDto toLocationFullDto(Location location);
+    Location toLocation(NewLocationDto newLocationDto);
 }
